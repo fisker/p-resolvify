a57ff87f89c0a28c882fe7da6e4e084bbefe77f2
fisker <lionkay@gmail.com>
2018-10-26 18:52:18 +0800

    umd support

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
-const identity = x => x
-const returnThis = x => () => x
-const isFunction = x => typeof x === 'function'
-const toFunction = x => isFunction(x) ? x : returnThis(x)
-const isThenable = x => {
-  return x !== null && typeof x === 'object' && typeof x.then === 'function'
-}
-const resolve = (x, handler) => {
-  return isThenable(x)
-    ? x.then(null, err => resolve(handler(err)))
-    : x
-}
-
-function resolvify (x, handler = identity) {
-  handler = toFunction(handler)
-
-  if (isFunction(x)) {
-    return function () {
-      return resolve(x.apply(this, arguments), handler)
-    }
+;(function(factory) {
+  const root = Function('return this')()
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return factory()
+    })
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory()
+  } else {
+    root.resolvify = factory()
+  }
+})(function() {
+  const identity = x => x
+  const returnThis = x => () => x
+  const isFunction = x => typeof x === 'function'
+  const toFunction = x => isFunction(x) ? x : returnThis(x)
+  const isThenable = x => {
+    return x !== null && typeof x === 'object' && typeof x.then === 'function'
+  }
+  const resolve = (x, handler) => {
+    return isThenable(x)
+      ? x.then(null, err => resolve(handler(err)))
+      : x
   }
 
-  return resolve(x, handler)
-}
+  function resolvify (x, handler = identity) {
+    handler = toFunction(handler)
 
-module.exports = resolvify
+    if (isFunction(x)) {
+      return function () {
+        return resolve(x.apply(this, arguments), handler)
+      }
+    }
+
+    return resolve(x, handler)
+  }
+
+  return resolvify
+})
